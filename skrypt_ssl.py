b4e5f64d99ee99e1f6db087eeae090f17a30b054
Ewa Miklewska <92916895+Ewa-M@users.noreply.github.com>
2024-06-13 18:50:38 +0200

    Changed to bearer token

--- skrypt_ssl.py
+++ skrypt_ssl.py
@@ -14,8 +14,9 @@ async def send_request(url, session, request_id):
         print(f"{request_id}; {url}; {str(e)}")
         return None
 
-async def main(url, num_requests, ssl_context):
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+async def main(url, num_requests, token):
+    headers = {'Authorization': token}
+    async with aiohttp.ClientSession(headers=headers) as session:
         tasks = []
         for i in range(num_requests):
             task = send_request(url, session, i + 1)
@@ -28,16 +29,9 @@ if _name_ == "_main_":
     parser = argparse.ArgumentParser(description="Test an API using aiohttp with SSL certificates")
     parser.add_argument("url", type=str, help="The URL to test")
     parser.add_argument("num_requests", type=int, help="Number of requests to send to the URL")
-    parser.add_argument("ca_cert", type=str, help="Path to CA certificate file")
-    parser.add_argument("client_cert", type=str, help="Path to client certificate file")
-    parser.add_argument("client_key", type=str, help="Path to client key file")
+    parser.add_argument("token", type=str, help="Bearer token")
     
     args = parser.parse_args()
 
-    # SSL context setup
-    ssl_context = ssl.create_default_context(cafile=certifi.where())
-    ssl_context.load_cert_chain(certfile=args.client_cert, keyfile=args.client_key)
-    ssl_context.load_verify_locations(cafile=args.ca_cert)
-
     # Run the asynchronous tasks
-    asyncio.run(main(args.url, args.num_requests, ssl_context))
+    asyncio.run(main(args.url, args.num_requests, args.token))
